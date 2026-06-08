a2fe08cf91fbea9271fc1e4696b70c3d30e28d4b
kalpesh <kalpesh162.com>
2026-06-07 20:05:04 -0700

    student-mangment -1

---
+++ student-management/student-management/src/main/java/com/mgm/student_management/entity/Student.java
@@ -0,0 +1,44 @@
+package com.mgm.student_management.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    private int age;
+    private String course;
+
+    public Student() {}
+
+    public Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+}
+
+
---
+++ student-management/student-management/src/main/java/com/mgm/student_management/exception/StudentNotFoundException.java
@@ -0,0 +1,14 @@
+package com.mgm.student_management.exception;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException() {
+	
+	}
+	
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+}
